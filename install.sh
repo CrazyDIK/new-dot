526ffa855d890e2b89f5681277fdbbc4c3aa1671
dkadmin <klishin.lip@yandex.ru>
2026-02-05 10:44:12 +0300

    mod install-1

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo pacman -Sy --noconfirm zsh git firefox eza bat stow alacritty fzf neovim np
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" -y
 
 rm -rf ~/.config/hypr
 rm -rf ~/.config/kitty
